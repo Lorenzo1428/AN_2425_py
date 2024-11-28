d0570f0dcbe27e99a7e913aff8c1de365440fecd
Lorenzo <patrizi.lorenzo12@gmail.com>
2024-11-28 23:44:37 +0100

    commit 4 (LU)

--- foglio1.py
+++ foglio1.py
@@ -1,8 +1,7 @@
 import numpy as np
-import metodi_numerici as m
-f = m.fattorizzazioni()
-A = 2
-A,B = f.lu_(A)
-
-
-print(A,B)
+from metodi_numerici import fattorizzazioni as f
+n = 3
+A = np.random.rand(n,n)
+A = np.matmul(A,A.transpose())
+L,U = f.lu_(A)
+print(np.linalg.norm( np.matmul(L,U) - A , np.inf) )
--- metodi_numerici.py
+++ metodi_numerici.py
@@ -3,5 +3,14 @@ import numpy as np
 class fattorizzazioni:
     @staticmethod
     def lu_(A):
-        B = A + 1
-        return A ,B
+        n = np.size(A,1)
+        n = 3
+        L = np.eye(n)
+        U = np.zeros((n,n))
+        for i in range(0,n):
+            for j in range(i,n):
+                U[i,j] = A[i,j] - np.dot(L[i,0:i],U[0:i,j])
+            for k in range(i+1,n):
+                L[k,i] = A[k,i] - np.dot(L[k,0:i],U[0:i,i])
+                L[k,i]= L[k,i]/U[i,i]
+        return L,U
