da27a9f62bf01e3c22cbcee8a857768065ac124f
Lorenzo <patrizi.lorenzo12@gmail.com>
2024-11-29 23:26:13 +0100

    Band LU and Chol

--- numerical_methods.py
+++ numerical_methods.py
@@ -17,6 +17,20 @@ class fattorizzazioni:
         return L,U
     
     @staticmethod
+    def lu_band(A,p,q):
+        '''Finds the LU factorization optimized for band matrices'''
+        n = np.size(A,1)
+        L = np.eye(n)
+        U = np.zeros((n,n))
+        for i in range(0,n):
+            for j in range(i,min(i+p+1,n)):
+                U[i,j] = A[i,j] - np.dot(L[i,max(0,i-q):i],U[max(0,i-p):i,j])
+            for k in range(i+1,min(i+q+1,n)):
+                L[k,i] = A[k,i] - np.dot(L[k,max(0,k-q):i],U[max(0,k-p):i,i])
+                L[k,i]= L[k,i]/U[i,i]
+        return L,U
+    
+    @staticmethod
     def chol_(A):
         '''Finds the matrix H' of Cholesky factorization'''
         n = np.size(A,1)
@@ -28,6 +42,20 @@ class fattorizzazioni:
                 H[i,k] = A[i,k] - np.dot(H[i,0:k],H[k,0:k])
 
                 H[i,k] = H[i,k]/H[k,k]
+        return H
+    
+    @staticmethod
+    def chol_band(A,q):
+        '''Finds the Cholesky factorization optimized for band matrices'''
+        n = np.size(A,1)
+        H = np.zeros((n,n))
+        for k in range(0,n):
+            H[k,k] = A[k,k] - np.dot(H[k,max(0,k-q):k],H[k,max(0,k-q):k])
+            H[k,k] = np.sqrt(H[k,k])
+            for i in range(k+1,min(n,k+q+1)):
+                H[i,k] = A[i,k] - np.dot(H[i,max(0,k-q):k],H[k,max(0,k-q):k])
+
+                H[i,k] = H[i,k]/H[k,k]        
         return H
 
     @staticmethod
@@ -56,6 +84,32 @@ class fattorizzazioni:
             x[k] = x[k]/U[k,k]
         return x
 
+    @staticmethod
+    def forwband(L,b,q):
+        '''Solves the lower triangular system given a nxn-dim band lower triangular matrix L and a n-dim vector b'''
+        n = np.size(L,1)
+        y = np.zeros((n,1))
+        y[0] = b[0]/L[0,0]
+        for k in range(1,n):
+            l_k = L[k,max(0,k-q):k]
+            y_k = y[max(0,k-q):k]
+            y[k] = b[k] - np.dot(l_k,y_k)
+            y[k] = y[k]/L[k,k]
+        return y
+
+    @staticmethod
+    def backband(U,y,p):
+        '''Solves the upper triangular system given a nxn-dim band upper triangular matrix U and a n-dim vector y'''
+        n = np.size(U,1)
+        x = np.zeros((n,1))
+        x[n-1] = y[n-1]/U[n-1,n-1]
+        for k in range(n-1,-1,-1):
+            u_k = U[k,k+1:min(n,k+p)]
+            x_k = x[k+1:min(n,k+p)]
+            x[k] = y[k] - np.dot(u_k,x_k)
+            x[k] = x[k]/U[k,k]
+        return x    
+    
     @classmethod
     def lu_2(cls,A,b):
         '''Solves the system using LU factorization'''
@@ -71,3 +125,4 @@ class fattorizzazioni:
         y = cls.forwardraw(H,b)
         x = cls.backwardraw(H.transpose(),y)
         return x
+    
--- sheet1.py
+++ sheet1.py
@@ -1,11 +1,17 @@
 import numpy as np
+import time 
 from numerical_methods import fattorizzazioni as f
-n = 3
+
+
+n = 500
 A = np.random.rand(n,n)
 A = np.matmul(A,A.transpose())
-#A =  2*np.eye(n) - (np.diag(np.ones((n-1)),-1) + np.diag(np.ones((n-1)), +1))
+A =  2*np.eye(n) - (np.diag(np.ones((n-1)),-1) + np.diag(np.ones((n-1)), +1))
 b = np.random.rand(n,1)
+t = time.time()
 H = f.chol_(A)
-x = f.chol_2(A,b)
-H1 = np.linalg.cholesky(A)
-print(np.linalg.norm(np.matmul(A,x) - b, np.inf))
+print("elapsed time: ", time.time() - t )
+t = time.time()
+Hb = f.chol_band(A,1)
+print("elapsed time: ", time.time() - t )
+print(np.linalg.norm(H - Hb, np.inf))
